14b4bdcc472082bd809599ccf3559425cb9e887a
junappleseed <junappleseed@gmail.com>
2016-01-09 23:57:38 +0900

    構成の変更
    構成変更のため不要なファイルを削除しました。

--- TimeLineSample/ViewController.swift
+++ TimeLineSample/ViewController.swift
@@ -9,24 +9,9 @@
 import UIKit
 
 class ViewController: UITabBarController {
-    
-    var feedTabView: FeedTabViewController!
-    var allTabView: AllPostTabViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        /* フィードタブ */
-        feedTabView = FeedTabViewController()
-        feedTabView.tabBarItem = UITabBarItem(title: "フィード", image: UIImage(named: "feed.png"), tag: 1)
-        /* すべての投稿タブ */
-        allTabView = AllPostTabViewController()
-        allTabView.tabBarItem = UITabBarItem(title: "すべて", image: UIImage(named: "post.png"), tag: 2)
-        
-        let feedTabViewController = UINavigationController(rootViewController: feedTabView)
-        let allTabViewController = UINavigationController(rootViewController: allTabView)
-
-        self.setViewControllers([feedTabViewController, allTabViewController], animated: false)
     }
 
     override func didReceiveMemoryWarning() {
